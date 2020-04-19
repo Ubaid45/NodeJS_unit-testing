75854f5080551c89af4e9cb5f1c27e382e9cb4e9
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-19 13:58:42 +0200

    Testing numbers

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,7 +1,19 @@
-test('Our first test', () => {
-
+const lib = require("../lib");
+test('absolute - should return positive number if the input is positive.', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
 
-test('Our second test', () => {
+test('absolute - should return positive number if the input is negative.', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return zero if the input is zero.', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+});
+
+/*test('Our second test', () => {
     throw new Error("Error");
-});
+});*/
